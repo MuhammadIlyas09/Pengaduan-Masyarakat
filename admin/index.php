f48167416caad20000c175b2a254a396f4ff4805
Muhamdtaopik <130655979+Muhamdtaopik@users.noreply.github.com>
2023-05-25 15:42:17 +0700

    mengganti title

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@
   <!DOCTYPE html>
   <html>
     <head>
-    	<title>Aplikasi Pengaduan masyarakat</title>
+    	<title>Website Pengaduan masyarakat</title>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
